529771f7481a528f2b6166c938aa2fe612c9c999
jeancmr <jean.madiedo08@gmail.com>
2025-04-21 22:58:43 -0500

    add business logic: cross schedule conflicts when submitting a class for teachers

--- services/clase.service.js
+++ services/clase.service.js
@@ -1,16 +1,15 @@
 const boom = require('@hapi/boom');
 const { models } = require('./../libs/sequelize');
+const { Op } = require('sequelize');
 
 class ClasesService {
   constructor() {}
 
   async create(data) {
-    console.log('data clase service ', data);
     const profesor = await models.Usuario.findByPk(data.profesorId);
-    if (!profesor || profesor.rol !== 'profesor') {
-      throw boom.notFound('Profesor not found');
-    }
+    if (!profesor || profesor.rol !== 'profesor') throw boom.notFound('Profesor not found');
 
+    await this.crossScheduleProfessor(data);
     const newClase = await models.Clase.create(data);
     return newClase;
   }
@@ -39,6 +38,20 @@ class ClasesService {
     const clase = await this.findOne(id);
     await clase.destroy();
     return { id };
+  }
+
+  async crossScheduleProfessor(data) {
+    const { profesorId, fecha, horaInicio, horaFin } = data;
+
+    const isConflict = await models.Clase.findOne({
+      where: {
+        profesorId,
+        fecha,
+        [Op.and]: [{ horaInicio: { [Op.lt]: horaFin } }, { horaFin: { [Op.gt]: horaInicio } }],
+      },
+    });
+
+    if (isConflict) throw boom.conflict('El profesor ya tiene una clase programada en ese horario');
   }
 }
 
